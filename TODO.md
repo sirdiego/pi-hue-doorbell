e94927048058af03e302fc200f55fbfc9d8b98b2
diego <diego@sirdiego.de>
2016-09-26 23:04:20 +0200

    Add logging to syslog and Rollbar

--- TODO.md
+++ TODO.md
@@ -1,3 +1,6 @@
 * http://pinout.xyz/ "Pinout"
 * ext-mmap https://github.com/calcinai/php-ext-mmap
 * BCM2708 seems to be BCM2835
+* documentation
+** php7
+** supervisor
--- app.php
+++ app.php
@@ -4,6 +4,16 @@ $config = include __DIR__ . '/config.php';
 
 use Calcinai\PHPi\Pin;
 use Calcinai\PHPi\Pin\PinFunction;
+use Monolog\Logger;
+use Monolog\Handler\SyslogHandler;
+use Monolog\Handler\RollbarHandler;
+
+$log = new Logger('pi-hue-doorbell');
+$log->pushHandler(new SyslogHandler('pi-hue-doorbell'));
+if(isset($config['rollbar'])) {
+    Rollbar::init($config['rollbar']);
+    $log->pushHandler(new RollbarHandler(Rollbar::$instance));
+}
 
 $loop = \React\EventLoop\Factory::create();
 $board = \Calcinai\PHPi\Factory::create($loop);
@@ -14,14 +24,18 @@ $client = new \GuzzleHttp\Client();
 $timer = new \stdclass;
 $timer->active = false;
 
-$loop->addPeriodicTimer(0.1, function () use ($pin, $client, $config, $timer) {
+$loop->addPeriodicTimer(0.1, function () use ($pin, $client, $config, $timer, $log) {
     if($pin->getLevel() && !$timer->active) {
-        $timer->active = true;
-        $resp = $client->request('PUT', 'http://' . $config['hue_bridge_ip'] . '/api/' . $config['hue_bridge_user'] . '/lights/'.$config['hue_light_id'].'/state', [
-            'json' => ['alert' => 'lselect']
-        ]);
-        echo $resp->getStatusCode() . PHP_EOL;
-        echo $resp->getBody() . PHP_EOL;
+        try {
+            $timer->active = true;
+            $resp = $client->request('PUT', 'http://' . $config['hue_bridge_ip'] . '/api/' . $config['hue_bridge_user'] . '/lights/'.$config['hue_light_id'].'/state', [
+                'json' => ['alert' => 'lselect']
+            ]);
+            echo $resp->getStatusCode() . PHP_EOL;
+        } catch (GuzzleHttp\Exception\TransferException $e) {
+            $timer->active = false;
+            $log->error($e->getMessage());
+        }
     }
 });
 
